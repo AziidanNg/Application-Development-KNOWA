6b0232d29c1023a78e71d405506763fd0dc22d0b
AziidanNg <aziidanng@gmail.com>
2026-01-11 19:51:26 +0800

    Fix chatbot faq issue

--- knowa_backend/chatbot/views.py
+++ knowa_backend/chatbot/views.py
@@ -4,25 +4,29 @@ from .models import FAQ
 from .serializers import FAQSerializer
 
 class FAQListView(generics.ListAPIView):
-    permission_classes = [permissions.IsAuthenticated]
+    # 1. Allow anyone (including guests) to read the FAQs
+    permission_classes = [permissions.AllowAny]
     serializer_class = FAQSerializer
 
     def get_queryset(self):
         user = self.request.user
         
-        # 1. Base rule: Show questions meant for everyone
+        # 2. Base rule: ALWAYS show questions meant for 'all'
         filters = Q(target_role='all')
 
-        # 2. Add specific roles based on user status
-        if user.is_staff or user.is_superuser:
-            filters |= Q(target_role='organizer')
-        
-        # NOTE: If you have a specific 'is_crew' field or group, add it here:
-        # if user.groups.filter(name='Crew').exists():
-        #     filters |= Q(target_role='crew')
-        
-        # If not staff (and assuming not crew), you are a participant
-        if not user.is_staff:
+        # 3. Check if user is actually logged in
+        if user.is_authenticated:
+            # --- LOGGED IN LOGIC ---
+            if user.is_staff or user.is_superuser:
+                # Staff see 'all' + 'organizer'
+                filters |= Q(target_role='organizer')
+            else:
+                # Regular members see 'all' + 'participant'
+                filters |= Q(target_role='participant')
+        else:
+            # --- GUEST LOGIC (Not Logged In) ---
+            # Guests see 'all' + 'participant' (assuming "how to join" info is for participants)
+            # You can remove the line below if you ONLY want them to see 'all'
             filters |= Q(target_role='participant')
 
         return FAQ.objects.filter(filters).distinct()
